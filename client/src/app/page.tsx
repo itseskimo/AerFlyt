8c94cf81443d37c455bdf5c499c3ec7245e4bae7
itseskimo <ayushmatmotgill@gmail.com>
2023-11-04 21:58:12 +0530

    update: otpless integration

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -10,6 +10,9 @@ export default function Home() {
     // Define the 'otpless' function
     window.otpless = (otplessUser) => {
       // Retrieve the user's details after successful login
+      alert(JSON.stringify(otplessUser));
+
+
       const waName = otplessUser.waName;
       const waNumber = otplessUser.waNumber;
 
