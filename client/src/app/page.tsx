2fb1e81d8928e2ec5f4930d3e2c91a9b4aee5056
itseskimo <ayushmatmotgill@gmail.com>
2023-11-08 15:52:14 +0530

    update:card

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -13,38 +13,38 @@ export default function Home() {
 
 
 
-      <div className="grid grid-cols-[repeat(auto-fit,minmax(150px,1fr))] md:grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4 sm:gap-8 mx-8">
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(150px,1fr))] md:grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-4 sm:gap-8 mx-8">
 
-{[0,1,3,4].map((item)=>(
-  <div key={item} className="border-solid border-[0.5px] border-gray-400 rounded-[2rem] flex flex-col  p-5">
-          <img src="./img/country.png" />
-          
-<div>
-<h1 className="text-[1.5rem] font-bold">Singapore</h1>
-<div>
-<span className="text-[0.875rem] text-[#5C5F5F]">VISA delivery by</span>
-<span className="text-visaBlue font-semibold">17th Oct | 12:30 PM</span>
-</div>
+        {[0, 1, 3, 4].map((item) => (
+          <div key={item} className="border-solid border-[0.5px] border-gray-400 rounded-[2rem] flex flex-col  p-5">
+            <img src="./img/country.png" />
 
-<section className="flex justify-between ">
-<div>
-<span className="text-[0.875rem] text-visaBlue">FARE (₹)</span>
-<span className=" font-semibold">Starting from</span>
-</div>
-<div>
-<span className="text-[0.875rem] text-visaBlue">VISA delivery by</span>
-<span className=" font-semibold">(includes all govt. related fees)</span>
-</div>
-</section>
+            <div className="py-5">
+              <h1 className="text-[1.5rem] font-bold">Singapore</h1>
+              <div className="flex items-center justify-between">
+                <span className="text-[0.875rem] text-[#5C5F5F]">VISA delivery by</span>
+                <span className="text-visaBlue font-semibold">17th Oct | 12:30 PM</span>
+              </div>
 
-
+              <section className="flex justify-between mt-3">
+                <div className="flex flex-col">
+                  <span className="text-[1.25rem] text-visaBlue font-bold">FARE (₹)</span>
+                  <span className=" text-[0.875rem] text-[#5C5F5F]">Starting from</span>
+                </div>
+                <div className="flex flex-col">
+                  <span className="text-[1.75rem] text-visaBlue font-bold">₹ 4,080</span>
+                  <span className=" text-[0.5rem] text-[#5C5F5F]">(includes all govt. related fees)</span>
+                </div>
+              </section>
 
 
-</div>
 
-        </div>
-))}
-        
+
+            </div>
+            <button className="bg-visaBlue text-white font-bold py-3">Apply for E-Visa</button>
+          </div>
+        ))}
+
 
       </div>
 
--- client/src/components/sections/Landing/HeroSection/HeroSection.tsx
+++ client/src/components/sections/Landing/HeroSection/HeroSection.tsx
@@ -24,11 +24,8 @@ const HeroSection = () => {
               <span className="bg-regal-purple shadow-elementShadow text-white py-[0.75rem] px-[2rem] rounded-[2rem] ">XX happy customers</span>
             </div>
 
-          </div>
 
-          <section className="flex items-center justify-between mt-20 mb-10">
-
-            <div className="flex items-center relative w-[60%]">
+            <div className="flex items-center relative w-[60%] mt-16 mb-10">
               <svg className="absolute  w-10 left-4 cursor-pointer" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <g clipPath="url(#clip0_987_3378)">
                   <path d="M17.5808 15.9434C18.8002 14.3748 19.5263 12.4037 19.5263 10.2631C19.5263 5.14724 15.379 1 10.2632 1C5.14725 1 1 5.14724 1 10.2631C1 15.379 5.14725 19.5262 10.2632 19.5262C12.4037 19.5262 14.3748 18.8002 15.9433 17.5809L21.0234 22.6609C21.4755 23.113 22.2087 23.113 22.6609 22.6609C23.113 22.2087 23.113 21.4755 22.6609 21.0234L17.5808 15.9434Z" stroke="black" strokeWidth="2" strokeLinecap="square" />
@@ -45,8 +42,31 @@ const HeroSection = () => {
                 placeholder="Where to?"
               />
             </div>
+          </div>
 
-            <div className="flex items-center relative w-[35%]">
+
+          
+
+         
+
+
+          <div className="flex items-center justify-center">
+            <button className="bg-[#FFA500] text-[1.25rem] font-bold  py-[1rem] px-[4.5rem] rounded-[2rem]">Apply for E-Visa</button>
+          </div>
+
+
+        </div>
+      </main>
+
+    
+    </>
+  )
+}
+
+export default HeroSection
+
+
+   {/* <div className="flex items-center relative w-[35%]">
               <svg className="absolute  w-10 left-4 cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                 <path d="M16 11C17.66 11 18.99 9.66 18.99 8C18.99 6.34 17.66 5 16 5C14.34 5 13 6.34 13 8C13 9.66 14.34 11 16 11ZM8 11C9.66 11 10.99 9.66 10.99 8C10.99 6.34 9.66 5 8 5C6.34 5 5 6.34 5 8C5 9.66 6.34 11 8 11ZM8 13C5.67 13 1 14.17 1 16.5V18C1 18.55 1.45 19 2 19H14C14.55 19 15 18.55 15 18V16.5C15 14.17 10.33 13 8 13ZM16 13C15.71 13 15.38 13.02 15.03 13.05C15.05 13.06 15.06 13.08 15.07 13.09C16.21 13.92 17 15.03 17 16.5V18C17 18.35 16.93 18.69 16.82 19H22C22.55 19 23 18.55 23 18V16.5C23 14.17 18.33 13 16 13Z" fill="#222222" />
               </svg>
@@ -71,21 +91,4 @@ const HeroSection = () => {
 
 
               </div>
-            </div>
-
-          </section>
-
-          <div className="flex items-center justify-center">
-            <button className="bg-[#FFA500] text-[1.25rem] font-bold  py-[1rem] px-[4.5rem] rounded-[2rem]">Apply for E-Visa</button>
-          </div>
-
-
-        </div>
-      </main>
-
-    
-    </>
-  )
-}
-
-export default HeroSection
+            </div> */}
