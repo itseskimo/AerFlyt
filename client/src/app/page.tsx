a9b758c3d1ec5d80d536e2553141cca33e3c0c87
itseskimo <ayushmatmotgill@gmail.com>
2023-11-11 21:34:42 +0530

    update: resolving ts issue

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -6,26 +6,27 @@ import VisaStepsHeader from '@/components/sections/Landing/VisaSteps/VisaStepsHe
 import VisaStepsDetails from "@/components/sections/Landing/VisaSteps/VisaStepsDetails"
 import CountryCard from '@/components/cards/CountryCard/CountryCard'
 
-export default function Home() {
+const Home: React.FC = () => {
+  const country = ["/img/dubai.avif", "/img/australia.avif", "/img/egypt.avif", "/img/thailand.avif", "/img/country.png", "/img/myanmar.png", "/img/srilanka.png"];
 
-  const country = ["/img/dubai.avif", "/img/australia.avif", "/img/egypt.avif", "/img/thailand.avif", "/img/country.png", "/img/myanmar.png", "/img/srilanka.png"]
- 
   return (
     <>
       <Header />
       <HeroSection />
       <ContinentSelector />
       <div className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-4 sm:gap-8 mx-8">
-        {country.map((item,idx) => (
-         <CountryCard key={idx} />
+        {country.map((item, idx) => (
+          <CountryCard key={idx} location={item} />
         ))}
       </div>
       <VisaStepsHeader />
-      <VisaStepsDetails /> 
+      <VisaStepsDetails />
       <div className="bg-[#7214ff0d] h-28 flex items-center justify-center font-bold text-[1.5rem]">
         <h1>Relax and start planning your trip 🎉</h1>
       </div>
       <Footer />
     </>
-  )
-}
+  );
+};
+
+export default Home;
