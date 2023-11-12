66ce7e3fd08da7ce835980161409a565f45c98be
itseskimo <ayushmatmotgill@gmail.com>
2023-11-13 00:15:39 +0530

    update: setting up signin page

---
+++ client/src/app/(pages)/sign-in/page.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/components/common/Navbar/Navbar"
+import Footer from "@/components/common/Footer/Footer"
+const page = () => {
+  return (
+    <>
+    <Navbar/>
+    <Footer/>    
+
+
+    </>
+  )
+}
+
+export default page
