e208609417fb38cf844d9aa33b4e2672b3b999c8
itseskimo <ayushmatmotgill@gmail.com>
2023-11-13 19:09:28 +0530

    update: navbar animation

--- client/src/components/common/Navbar/Navbar.tsx
+++ client/src/components/common/Navbar/Navbar.tsx
@@ -1,24 +1,48 @@
-import React from 'react'
+'use client'
+import React, { useState, useEffect } from 'react';
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
+  const [scrolling, setScrolling] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <header className="flex items-center gap-1 justify-center bg-[#222222] text-white py-2">
+     {/* <header className={`flex z-10  items-center gap-1 justify-center bg-[#222222] text-white py-2 transition-all duration-500 fixed top-20 left-0 w-full  ${!scrolling ? 'opacity-100' : 'opacity-0'}`}>
+        <span className="text-[#D0BCFF]">"20% OFF</span>
+        <span> when you book VISA for</span>
+        <span className="text-[#D0BCFF]">2+ travelers!</span>
+        <span>Save big on your journey now." </span>
+      </header> */}
+
+
+      <nav className={`flex z-10 items-center justify-between shadow-navshadow px-8 w-full fixed top-0 left-0 backdrop-blur-sm h-20 transition-all duration-500 ${scrolling && 'bg-white'}`}>
+        <button className="bg-regal-purple py-[0.625rem] px-[1.125rem] text-white rounded-[2rem]">Aerviz</button>
+        <button className="bg-[#222222] py-[0.625rem] px-[1.125rem] rounded-md text-white">Login</button>
+      </nav>
+
+      <header className={`flex z-10  items-center gap-1 justify-center bg-[#222222] text-white py-2 transition-all duration-500 fixed top-20 left-0 w-full  ${scrolling ? 'opacity-100' : 'opacity-0'}`}>
         <span className="text-[#D0BCFF]">"20% OFF</span>
         <span> when you book VISA for</span>
         <span className="text-[#D0BCFF]">2+ travelers!</span>
         <span>Save big on your journey now." </span>
       </header>
-
-      <nav className='flex z-10 items-center justify-between shadow-navshadow px-8  w-full backdrop-blur-sm h-20'>
-        <button className="bg-regal-purple py-[0.625rem] px-[1.125rem] text-white rounded-[2rem]">Aerviz</button>
-        <div>
-          <button className="mr-8 border-solid border-[1px] border-white font-semibold text-white py-[0.625rem] px-[1.125rem] rounded-md">For travel agents</button>
-          <button className="bg-[#222222] py-[0.625rem] px-[1.125rem] rounded-md text-white">Login</button>
-        </div>
-      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
