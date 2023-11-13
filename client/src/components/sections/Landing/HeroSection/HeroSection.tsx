9033c79438e84a7ecb5e964b356be26e8c5ba40d
itseskimo <ayushmatmotgill@gmail.com>
2023-11-13 18:47:14 +0530

    update: minor changes

--- client/src/components/sections/Landing/HeroSection/HeroSection.tsx
+++ client/src/components/sections/Landing/HeroSection/HeroSection.tsx
@@ -3,11 +3,9 @@ import React from 'react'
 const HeroSection = () => {
   return (
     <>
-        
-        <main className="bg-[url(/img/Landing/LandingBg.jpg)] bg-no-repeat bg-cover py-28 ">
+
+      <main className="bg-[url(/img/Landing/LandingBg.jpg)] bg-no-repeat bg-cover py-28 ">
         <div className="mx-8">
-
-
 
           <div className="flex flex-col gap-2 justify-center items-center cursor-default">
 
@@ -45,9 +43,9 @@ const HeroSection = () => {
           </div>
 
 
-          
 
-         
+
+
 
 
           <div className="flex items-center justify-center">
@@ -58,7 +56,7 @@ const HeroSection = () => {
         </div>
       </main>
 
-    
+
     </>
   )
 }
@@ -66,7 +64,7 @@ const HeroSection = () => {
 export default HeroSection
 
 
-   {/* <div className="flex items-center relative w-[35%]">
+{/* <div className="flex items-center relative w-[35%]">
               <svg className="absolute  w-10 left-4 cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                 <path d="M16 11C17.66 11 18.99 9.66 18.99 8C18.99 6.34 17.66 5 16 5C14.34 5 13 6.34 13 8C13 9.66 14.34 11 16 11ZM8 11C9.66 11 10.99 9.66 10.99 8C10.99 6.34 9.66 5 8 5C6.34 5 5 6.34 5 8C5 9.66 6.34 11 8 11ZM8 13C5.67 13 1 14.17 1 16.5V18C1 18.55 1.45 19 2 19H14C14.55 19 15 18.55 15 18V16.5C15 14.17 10.33 13 8 13ZM16 13C15.71 13 15.38 13.02 15.03 13.05C15.05 13.06 15.06 13.08 15.07 13.09C16.21 13.92 17 15.03 17 16.5V18C17 18.35 16.93 18.69 16.82 19H22C22.55 19 23 18.55 23 18V16.5C23 14.17 18.33 13 16 13Z" fill="#222222" />
               </svg>
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -21,6 +21,7 @@ const config: Config = {
 
       boxShadow: {
         navshadow: ' 0px 3px 16px 0px rgba(0, 0, 0, 0.25)',
+        authshadow: '0px 2px 12px 0px rgba(0, 0, 0, 0.10)',
         elementShadow: '0px 2px 3px 0px rgba(0, 0, 0, 0.10), 0px 6px 6px 0px rgba(0, 0, 0, 0.09), 0px 14px 8px 0px rgba(0, 0, 0, 0.05), 0px 24px 10px 0px rgba(0, 0, 0, 0.01), 0px 38px 11px 0px rgba(0, 0, 0, 0.00)',
         listBoxShadow: '0px 1px 2px 0px rgba(0, 0, 0, 0.10), 0px 4px 4px 0px rgba(0, 0, 0, 0.05), 0px 9px 5px 0px rgba(0, 0, 0, 0.02), 0px 16px 6px 0px rgba(0, 0, 0, 0.01), 0px 24px 7px 0px rgba(0, 0, 0, 0.00)'
       },
