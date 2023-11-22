6e228e9acf586b7652f4053e3f30f2c9f838d89f
itseskimo <ayushmatmotgill@gmail.com>
2023-11-22 19:53:15 +0530

    update: country details responsive

--- client/src/app/(pages)/countryDetails/page.jsx
+++ client/src/app/(pages)/countryDetails/page.jsx
@@ -22,8 +22,8 @@ const page = () => {
 
           <aside className=' flex flex-col gap-6 mt-[-60px] order-2 lg:order-1'>
 
-            <header className={`flex   items-center gap-1 justify-center bg-[#222222] text-white py-2 transition-all duration-500  w-full text-[0.6rem] sm:text-[0.7rem] md:text-[1rem] rounded-[1.25rem] border-solid border-[#7A757F] border-[0.5px]`}>
-              <span className="text-[#D0BCFF]">"20% OFF</span>
+            <header className={`hidden lg:flex   items-center gap-1 justify-center bg-[#222222] text-white p-2  w-full text-[0.875rem]  rounded-[1.25rem] border-solid border-[#7A757F] border-[0.5px]`}>
+              <span className="text-[#D0BCFF] font-bold">"20% OFF</span>
               <span> when you book VISA for</span>
               <span className="text-[#D0BCFF]">2+ travelers!</span>
               <span>Save big on your journey now." </span>
@@ -35,7 +35,7 @@ const page = () => {
             <DocsRequired />
           </aside>
 
-          <aside className='shadow-navshadow order-1 lg:order-2 bg-white  h-max  rounded-[1.5rem] flex flex-col gap-6   md:sticky top-14 p-5 md:mt-[-250px] '>
+          <aside className='shadow-navshadow order-1 lg:order-2 bg-white  h-max  rounded-[1.5rem] flex flex-col gap-6  lg:sticky top-14 p-5 lg:mt-[-250px] '>
 
             <main className='rounded-[1.5rem]  overflow-hidden shadow-paymentShadow border-[2px] border-solid border-visaBlue'>
 
